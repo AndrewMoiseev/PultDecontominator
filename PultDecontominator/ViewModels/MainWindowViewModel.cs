d00cd50905e7077091beb8e27a6c286b0a1c51cc
AndrewMoiseev <Andrew.Moiseev@gmail.com>
2018-05-05 10:51:18 +0500

    working in Main View

---
+++ PultDecontominator/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace PultDecontominator.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public bool IsEnabled
+        {
+            get { return _isEnabled; }
+            set
+            {
+                SetProperty(ref _isEnabled, value);
+                ExecuteDelegateCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public string UpdateText
+        {
+            get { return _updateText; }
+            set { SetProperty(ref _updateText, value); }
+        }
+
+
+        public DelegateCommand ExecuteDelegateCommand { get; private set; }
+
+        public DelegateCommand<string> ExecuteGenericDelegateCommand { get; private set; }        
+
+        public DelegateCommand DelegateCommandObservesProperty { get; private set; }
+
+        public DelegateCommand DelegateCommandObservesCanExecute { get; private set; }
+
+
+        public MainWindowViewModel()
+        {
+            ExecuteDelegateCommand = new DelegateCommand(Execute, CanExecute);
+
+            DelegateCommandObservesProperty = new DelegateCommand(Execute, CanExecute).ObservesProperty(() => IsEnabled);
+
+            DelegateCommandObservesCanExecute = new DelegateCommand(Execute).ObservesCanExecute(() => IsEnabled);
+
+            ExecuteGenericDelegateCommand = new DelegateCommand<string>(ExecuteGeneric).ObservesCanExecute(() => IsEnabled);
+        }
+
+        private void Execute()
+        {
+            UpdateText = $"Updated: {DateTime.Now}";
+        }
+
+        private void ExecuteGeneric(string parameter)
+        {
+            UpdateText = parameter;
+        }
+
+        private bool CanExecute()
+        {
+            return IsEnabled;
+        }
+        private bool _isEnabled;
+        private string _updateText;
+
+    }
+}
--- PultDecontominator/Views/MainWindow.xaml.cs
+++ PultDecontominator/Views/MainWindow.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System.Windows;
 
-namespace PultDecontominator
+namespace PultDecontominator.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
