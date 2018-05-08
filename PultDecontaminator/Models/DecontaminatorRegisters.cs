706ca2d0acfed72d7db056c1beb433ffa0239e10
AndrewMoiseev <Andrew.Moiseev@gmail.com>
2018-05-08 10:56:48 +0500

    List of registers decontominator

---
+++ PultDecontaminator/Models/DecontaminatorRegisters.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Specialized;
+
+namespace PultDecontominator.Models
+{
+    public class DecontaminatorRegister
+    {
+        public DecontaminatorRegister(string name, string panel, string typeData, int addresRegister, string description, string descriptionTypeData)
+        {
+            Name = name;
+            Panel = panel;
+            TypeData = typeData;
+            AddresRegister = addresRegister;
+            Description = description;
+            DescriptionTypeData = descriptionTypeData;
+        }
+
+        public string Name
+        {
+            get ; 
+            set ; 
+        }
+        public string Panel { get; }
+        public string TypeData
+        {
+            get;
+            set;
+        }
+        public int AddresRegister
+        {
+            get;
+            set;
+        }
+        public string Description
+        {
+            get;
+            set;
+        }
+        public string DescriptionTypeData
+        {
+            get;
+            set;
+        }
+        public ushort RegisterValue
+        {
+            get;
+            set;
+        }
+
+    }
+}
--- PultDecontaminator/ViewModels/MainWindowViewModel.cs
+++ PultDecontaminator/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Configuration;
+using System.IO;
 using System.Linq;
+using System.Web.UI;
 using Prism.Commands;
 using Prism.Mvvm;
+using PultDecontominator.Models;
 using PultDecontominator.Services;
 
 namespace PultDecontominator.ViewModels
@@ -18,6 +22,12 @@ namespace PultDecontominator.ViewModels
             get { return _decontominators; }
         }
 
+//        public ObservableCollection<DecontaminatorRegister> Registers
+        public List<DecontaminatorRegister> Registers
+        {
+            get { return _registers; }
+            set { SetProperty(ref _registers, value); }
+        }
         public bool IsEnabled
         {
             get { return _isEnabled; }
@@ -42,7 +52,8 @@ namespace PultDecontominator.ViewModels
         public DelegateCommand ExecuteStartDecontominationCommand { get; private set; }
         public DelegateCommand ExecuteStopCommand { get; private set; }
         public DelegateCommand ExecuteSendRegisterCommand { get; private set; }
-
+        public DelegateCommand ExecuteOpenComPortCommand { get; private set; }
+        public DelegateCommand ExecuteCloseComPortCommand { get; private set; }
         public DelegateCommand<string> ExecuteGenericDelegateCommand { get; private set; }        
 
         public DelegateCommand DelegateCommandObservesProperty { get; private set; }
@@ -53,14 +64,24 @@ namespace PultDecontominator.ViewModels
         public MainWindowViewModel()
         {
             _decontominators = new List<string>();
+
+            //_registers = new ObservableCollection<DecontaminatorRegister>(ReadCSV("1.csv"));
+            //_registers = new List<DecontaminatorRegister>(ReadCSV("1.csv"));
+
             //int value = Int32.Parse(ConfigurationManager.AppSettings["StartingMonthColumn"]);
             string _decs = ConfigurationManager.AppSettings["SlaveIds"];
             _decontominators = _decs.Split(',').ToList();
             
+
+
+
             ExecuteStartDecontominationCommand = new DelegateCommand(ExecuteStartDecontomination, CanExecute);
             ExecuteStartDryCommand = new DelegateCommand(ExecuteStartDry, CanExecute);
             ExecuteStopCommand = new DelegateCommand(ExecuteStop, CanExecute);
             ExecuteSendRegisterCommand = new DelegateCommand(ExecuteSendRegister, CanExecute);
+
+            ExecuteOpenComPortCommand = new DelegateCommand(ExecuteOpenComPort, CanExecute);
+            ExecuteCloseComPortCommand = new DelegateCommand(ExecuteCloseComPort, CanExecute);
 //            ExecuteDelegateCommand = new DelegateCommand(Execute, CanExecute);
 
             DelegateCommandObservesProperty = new DelegateCommand(Execute, CanExecute).ObservesProperty(() => IsEnabled);
@@ -71,6 +92,16 @@ namespace PultDecontominator.ViewModels
 
             AuthTrue = true;
             _isEnabled = AuthTrue;
+        }
+
+        private void ExecuteCloseComPort()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ExecuteOpenComPort()
+        {
+            throw new NotImplementedException();
         }
 
         private async void ExecuteStartDecontomination() => await _modbusService.StartDecontominationTask();
@@ -101,5 +132,45 @@ namespace PultDecontominator.ViewModels
         private ModbusService _modbusService = new ModbusService();
         private ushort _register;
         private List<string> _decontominators;
+        //private ObservableCollection<DecontaminatorRegister> _registers;
+        private List<DecontaminatorRegister> _registers = new List<DecontaminatorRegister>();
+        //public ObservableCollection<T> Convert<T>(IEnumerable<T> original)
+        //{
+        //    return new ObservableCollection<T>(original);
+        //}
+        public IEnumerable<DecontaminatorRegister> ReadCSV(string fileName)
+        {
+            // We change file extension here to make sure it's a .csv file.
+            // TODO: Error checking.
+            string[] lines = File.ReadAllLines(System.IO.Path.ChangeExtension(fileName, ".csv"));
+
+            return lines?.Select(line =>
+            {
+                string[] data = line.Split(';');
+                //Name = name;
+                //Panel = panel;
+                //TypeData = typeData;
+                //AddresRegister = addresRegister;
+                //Description = description;
+                //DescriptionTypeData = descriptionTypeData;
+                return new DecontaminatorRegister(data[0], data[1], data[2], Convert.ToInt32(data[3]), data[4],
+                    data[5]);
+            });
+        }
+    }
+    internal class ItemRegsViewModel : BindableBase
+    {
+        public ItemRegsViewModel(DecontaminatorRegister item)
+        {
+            _item = item;
+            
+        }
+
+        public string Name => _item.Name;
+        public string Description => _item.Description;
+        public int AddresRegister => _item.AddresRegister;
+
+
+        private  DecontaminatorRegister _item;
     }
 }
